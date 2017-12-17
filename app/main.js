7178b73b3a43e7bdecf9ea608ba3f3e976127efa
Tom Klaver <tomklav@gmail.com>
2017-12-17 18:04:12 +0100

    implementing new design WIP

--- app/main.js
+++ app/main.js
@@ -1,10 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("button_subscribe").addEventListener("click", function() {
-        document.getElementById("button_subscribe").setAttribute('disabled', 'disabled');
-        document.getElementById("button_subscribe").innerHTML = 'Thank you!';
-        var email = document.getElementById("input_email").value;
-        sendFormspreeMail({
-            email: email
+    // document.getElementById("button_subscribe").addEventListener("click", function() {
+    //     document.getElementById("button_subscribe").setAttribute('disabled', 'disabled');
+    //     document.getElementById("button_subscribe").innerHTML = 'Thank you!';
+    //     var email = document.getElementById("input_email").value;
+    //     sendFormspreeMail({
+    //         email: email
+    //     });
+    // });
+
+    var nav = document.getElementById("navigation");
+    var activeNavItem = nav.children[0].getAttribute('href');
+
+    function activateClass(className) {
+        var elms = document.getElementsByClassName('toggle');
+        for (var i = 0; i < elms.length; i++) {
+            elms[i].classList.remove('active');
+        }
+        elms = document.getElementsByClassName(className);
+        for (var i = 0; i < elms.length; i++) {
+            elms[i].classList.add('active');
+        }
+    }
+
+    for (var i = 0; i < nav.children.length; i++) {
+        nav.children[i].addEventListener('click', function(e) {
+            e.preventDefault();
+            activateClass(this.getAttribute('href'));
+            return false;
         });
-    });
+    }
+
+    activateClass(activeNavItem);
 });
--- dist/formspree.js
+++
@@ -1,8 +0,0 @@
-var url = '//formspree.io/admin@sportyspots.com';
-
-function sendFormspreeMail(obj) {
-    var req = new XMLHttpRequest();
-    req.open("POST", url, true);
-    req.setRequestHeader("Content-type", "application/json");
-    req.send(JSON.stringify(obj));
-}
