400a46238df88b60abd37e7380590eb443f247ed
Sezayi <arslan.sezayi@gmail.com>
2018-02-21 15:17:06 +0100

    sez updates

--- app/main.js
+++ app/main.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // document.getElementById('button_subscribe').addEventListener('click', function() {
-  //     document.getElementById('button_subscribe').setAttribute('disabled', 'disabled');
-  //     document.getElementById('button_subscribe').innerHTML = 'Thank you!';
-  //     var email = document.getElementById('input_email').value;
-  //     sendFormspreeMail({
-  //         email: email
-  //     });
-  // });
+  
+  document.getElementById('button_subscribe').addEventListener('click', function() {
+      document.getElementById('button_subscribe').setAttribute('disabled', 'disabled');
+      document.getElementById('button_subscribe').innerHTML = 'Top! Dank voor je aanmelding';
+      var email = document.getElementById('input_email').value;
+      var url = "https://script.google.com/macros/s/AKfycbwcXYjElC-P6qyAl1BNPItw3PSJa9pG1_HFeK7JlO3Qo1ZVwC84/exec?email=" + encodeURI(email)
+      var req = new XMLHttpRequest();
+      req.open("GET", url, true);
+      req.send();
+  });
 
   var nav = document.getElementById('navigation');
   var activeNavItem = nav.children[0].getAttribute('href');
@@ -20,6 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
     for (var i = 0; i < elms.length; i++) {
       elms[i].classList.add('active');
     }
+    document.body.setAttribute('class', className);
   }
 
   for (var i = 0; i < nav.children.length; i++) {
